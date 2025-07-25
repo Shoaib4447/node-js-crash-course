e07d3631c00a283d49bceef89396de18eae31531
shoaib bhatti <shoaiburrehman4447@gmail.com>
2025-07-25 22:33:56 +0500

    deal with req object and simple basic routes

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 import http from "http";
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("content-Type", "text/html");
+  // Dealing with res object
+
   //   res.setHeader("content-Type", "text/plain");
+  //   res.setHeader("content-Type", "text/html");
+  //   res.writeHead(500, "content-Type", "application/json");
+  //   res.write(JSON.stringify({ message: "server error" }));
   //   res.write("Hello Node.js");
-  res.writeHead(500, "content-Type", "application/json");
-  res.write(JSON.stringify({ message: "server error" }));
-  res.end();
+
+  // Dealing with req object
+  console.log(req.url);
+  console.log(req.method);
+
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Hello Homepage URL</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Hello AboutPage URL</h1>");
+      } else {
+        res.writeHead(400, { "content-type": "text/html" });
+        res.end("<h1>Page Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method Not Allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/plain" });
+    res.write("server error");
+    res.end();
+  }
 });
 
-server.listen(8000, () => {
-  console.log("server running on port : 8000");
+server.listen(PORT, () => {
+  console.log(`server running on port : ${PORT}`);
 });
